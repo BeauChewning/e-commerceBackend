f94a5289c17f72d18dfbc8f593ada56f729e9085
BeauChewning <beauchewning96@gmail.com>
2021-10-29 23:27:15 -0400

    seeded file

--- models/Category.js
+++ models/Category.js
@@ -17,38 +17,7 @@ Category.init(
       allowNull: false
     }
   },
-  //product
-  {
-    id:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_name:{
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price:{
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      isDecimal: true//this one
-    },
-    stock:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      is: 10,
-      isNumeric: true
-    },
-    category_id:{
-      type: DataTypes.INTEGER,
-      refrences:{
-        model: "Category",
-        key: "id"
-      }
-    },
-  },
-  
+ 
   {
     sequelize,
     timestamps: false,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -74,18 +74,18 @@ router.post('/', (req, res) => {
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
-        const Arr = req.body.tagIds.map((tag_id) => {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(Arr);
+        return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
       res.status(200).json(product);
     })
-    .then((productTag) => res.status(200).json(productTag))
+    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -127,7 +127,7 @@ router.put('/:id', (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((ProductTags) => res.json(ProductTags))
+    .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
       res.status(400).json(err);
